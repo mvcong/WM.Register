41cfdce0ce19a34c016cda3f5da7bef5ae6d6bac
mvcong <45279457+mvcong@users.noreply.github.com>
2019-05-06 21:46:01 +0700

    Revert ".gitignore"
    This reverts commit 389a8ab4b2c0b63bf399a273940f4252cbef1d8f.

---
+++ WM.Register/ServerMail/EmailManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace WM.Register.ServerMail
+{
+    public class EmailManager
+    {
+        public static void AppSettings(out string FromEmail, out string Password, out string SMTPPort, out string Host)
+        {
+            FromEmail = ConfigurationManager.AppSettings.Get("FromEmail");
+            Password = ConfigurationManager.AppSettings.Get("Password");
+            SMTPPort = ConfigurationManager.AppSettings.Get("SMTPPort");
+            Host = ConfigurationManager.AppSettings.Get("Host");
+        }
+        public static void SendEmail(string From, string Subject, string Body, string To, string FromEmail, string Password, string SMTPPort, string Host)
+        {
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.To.Add(To);
+            mail.From = new MailAddress(From);
+            mail.Subject = Subject;
+            mail.Body = Body;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = Host;
+            smtp.Port = Convert.ToInt16(SMTPPort);
+            smtp.Credentials = new NetworkCredential(FromEmail, Password);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+    }
+}
